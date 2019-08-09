05802eaceeb02770816a9545c5af0717892e1831
RodrigoRP <rodrigo.ramos.pa@gmail.com>
2019-08-09 15:14:09 -0300

    Add data

--- src/main/java/com/example/mongo/config/Instantiation.java
+++ src/main/java/com/example/mongo/config/Instantiation.java
@@ -23,6 +23,7 @@ public class Instantiation implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User jose = new User(null, "Joaquina Mendonca", "joaquina@bol.com.br");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
